cb5e0ad7773f3519af385064321068005621d503
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2025-05-17 16:13:25 -0500

    Se estan creando las ventanas necesarias

--- src/main/java/co/edu/uniquindio/alquiler/controller/InicioSesionController.java
+++ src/main/java/co/edu/uniquindio/alquiler/controller/InicioSesionController.java
@@ -1,4 +1,22 @@
 package co.edu.uniquindio.alquiler.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class InicioSesionController {
+
+    @FXML
+    private Label nombreUsuarioLabel;
+    @FXML
+    private PasswordField contrasenaPasswordField;
+    @FXML
+    private TextField nombreUsuarioTextfield;
+    @FXML
+    private Label contrasenaLabel;
+
+    public void initialize() {
+
+    }
 }
