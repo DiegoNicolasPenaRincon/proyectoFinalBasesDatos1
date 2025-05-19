4d7eb2d36564af7e2814978c0327c2d2b93bb67e
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-05-19 12:52:42 -0500

    bacaneria

---
+++ src/main/resources/ScriptsSQLserver/SELECT FORM.sql
@@ -0,0 +1,16 @@
+/*SELECT * FROM Administrador;
+SELECT * FROM Cajero;
+SELECT * FROM CategoriaProducto;
+*/
+SELECT * FROM Cliente;
+SELECT * FROM Factura;
+SELECT * FROM Factura_Producto;
+SELECT * FROM Inventario_Proveedor;
+SELECT * FROM Modificacion;
+SELECT * FROM Pedido;
+SELECT * FROM Pedido_Producto;
+SELECT * FROM Producto;
+SELECT * FROM Proveedor;
+SELECT * FROM Proveedor_Producto;
+SELECT * FROM Inventario;
+
